c3f05f7a23b61df951a9f65ac361b0c5a3f07e80
Sparsh Gaur <sparshgaur451@gmail.com>
2024-05-15 17:36:54 +0530

    Moving to production

---
+++ src/lib/constants.ts
@@ -0,0 +1,61 @@
+/*import BarChart from '@/components/icons/bar_chart'
+import Calendar from '@/components/icons/calendar'
+import CheckCircle from '@/components/icons/check_circled'
+import Chip from '@/components/icons/chip'
+import ClipboardIcon from '@/components/icons/clipboardIcon'
+import Compass from '@/components/icons/compass'
+import Database from '@/components/icons/database'
+import Flag from '@/components/icons/flag'
+import Headphone from '@/components/icons/headphone'
+import Home from '@/components/icons/home'
+import Info from '@/components/icons/info'
+import LinkIcon from '@/components/icons/link'
+import Lock from '@/components/icons/lock'
+import Message from '@/components/icons/messages'
+import Notification from '@/components/icons/notification'
+import Payment from '@/components/icons/payment'
+import Person from '@/components/icons/person'
+import Pipelines from '@/components/icons/pipelines'
+import PluraCategory from '@/components/icons/plura-category'
+import Power from '@/components/icons/power'
+import Receipt from '@/components/icons/receipt'
+import Send from '@/components/icons/send'
+import Settings from '@/components/icons/settings'
+import Shield from '@/components/icons/shield'
+import Star from '@/components/icons/star'
+import Tune from '@/components/icons/tune'
+import Video from '@/components/icons/video_recorder'
+import Wallet from '@/components/icons/wallet'
+import Warning from '@/components/icons/warning' */
+
+export const pricingCards = [
+  {
+    title: 'Starter',
+    description: 'Perfect for trying out plura',
+    price: 'Free',
+    duration: '',
+    highlight: 'Key features',
+    features: ['3 Sub accounts', '2 Team members', 'Unlimited pipelines'],
+    priceId: '',
+  },
+  {
+    title: 'Unlimited Saas',
+    description: 'The ultimate agency kit',
+    price: '$199',
+    duration: 'month',
+    highlight: 'Key features',
+    features: ['Rebilling', '24/7 Support team'],
+    priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+  },
+  {
+    title: 'Basic',
+    description: 'For serious agency owners',
+    price: '$49',
+    duration: 'month',
+    highlight: 'Everything in Starter, plus',
+    features: ['Unlimited Sub accounts', 'Unlimited Team members'],
+    priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+  },
+]
+
+
